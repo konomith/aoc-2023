99a839279645da12fd2438f6c4acb0ed70a43dc2
Hari <konomi.theohar@gmail.com>
2023-12-01 16:21:50 -0500

    Don't unnecessarily store all digits, make it possible to run only part 1 or both

--- src/bin/d1.rs
+++ src/bin/d1.rs
@@ -1,37 +1,25 @@
-use std::{env, println};
+use std::collections::HashMap;
+use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::collections::HashMap;
 
+fn process_line(line: &str) -> u32 {
+    let mut first: u32 = 0;
+    let mut last: u32 = 0;
 
-fn process_line(line: &str) -> i32 {
-    let filtered = line
-        .chars()
-        .filter(|c| c.is_digit(10))
-        .collect::<Vec<_>>();
-
-    let mut out = String::new();
-    match filtered.first() {
-        Some(c) => {out.push(*c);}
-        None => {}
-    }
-
-    match filtered.last() {
-        Some(c) => {out.push(*c);}
-        None => {}
-    }
-
-    match out.parse::<i32>() {
-        Ok(r) => {
-            r
-        }
-        Err(_) => {
-            0
+    for c in line.chars() {
+        if c.is_digit(10) {
+            let c_digit = c.to_digit(10).unwrap();
+            if first == 0 {
+                first = c_digit;
+            }
+            last = c_digit;
         }
     }
+    first * 10 + last
 }
 
-fn format_line(line: &mut String) -> String {
+fn format_line_for_part2(line: &mut String) -> String {
     let map = HashMap::from([
         ("one", "1"),
         ("two", "2"),
@@ -41,7 +29,7 @@ fn format_line(line: &mut String) -> String {
         ("six", "6"),
         ("seven", "7"),
         ("eight", "8"),
-        ("nine", "9")
+        ("nine", "9"),
     ]);
 
     let mut indices: Vec<(usize, &str)> = Vec::new();
@@ -52,11 +40,13 @@ fn format_line(line: &mut String) -> String {
     }
 
     indices.sort();
+
     let mut formatted = line.clone();
 
     match indices.first() {
         Some((_index, key)) => {
-            formatted = formatted.replacen(key, map.get(key).unwrap(), 1);
+            let new = format!("{}", map.get(key).unwrap());
+            formatted = formatted.replacen(key, &new, 1);
         }
         None => {}
     }
@@ -73,10 +63,12 @@ fn format_line(line: &mut String) -> String {
 fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
+    let all_parts = args.len() < 3;
+
     let file = File::open(file_path)?;
     let mut reader = BufReader::new(file);
 
-    let mut sum: i32 = 0;
+    let mut sum: u32 = 0;
 
     let mut line = String::new();
 
@@ -85,7 +77,9 @@ fn main() -> Result<(), std::io::Error> {
         if n == 0 {
             break;
         }
-        line = format_line(&mut line);
+        if all_parts {
+            line = format_line_for_part2(&mut line);
+        }
         let num = process_line(&line.trim());
         sum += num;
         line.clear()
